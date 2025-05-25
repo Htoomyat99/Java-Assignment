9dfab0732d5c9d68ab10a07d4d22eee168dbeeb1
Htoomyat99 <htoomyat.20399@gmail.com>
2025-05-25 20:08:15 +0630

    delete unnecessary file

--- src/com/soft/demo/MyPrinterOperation.java
+++
@@ -1,7 +0,0 @@
-package com.soft.demo;
-
-public class MyPrinterOperation {
-    public void printA4(String val) {
-        System.out.println("Printing A4" + val);
-    }
-}
--- src/com/soft/demo/MyPrinterSystem.java
+++
@@ -1,5 +0,0 @@
-package com.soft.demo;
-
-public final class MyPrinterSystem {
-    public  static MyPrinterOperation printerOperation = new MyPrinterOperation();
-}
